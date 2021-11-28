d15906f20cbeb742e1f2b763cdd51ecdc9a61c2d
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-11-28 22:23:31 +0100

    use it in the frontend

--- backend/fruitrient/app/app.py
+++ backend/fruitrient/app/app.py
@@ -1,8 +1,9 @@
 from falcon.asgi import App
+from falcon import CORSMiddleware
 from .handlers import Basic
 
 
 def create_app():
-    app = App()
+    app = App(middleware=CORSMiddleware())
     app.add_route('/', Basic())
     return app
--- backend/fruitrient/app/handlers.py
+++ backend/fruitrient/app/handlers.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 
 class Basic:
-    async def on_get(self, req: Request, resp: Response):
+    async def on_post(self, req: Request, resp: Response):
         body = await req.stream.read()
 
         logger.info(f"params: {req.params} body: {body}")
@@ -15,4 +15,4 @@ class Basic:
         resp.content_type = falcon.MEDIA_HTML
 
         # Just ping back the body
-        resp.text = body
+        resp.text = f"the backend received: \"{body}\""
