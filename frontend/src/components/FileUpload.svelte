08cea5cb62002f10a675b990019b0e4acb9469ae
Idrees <ahmadid@student.chalmers.se>
2021-12-06 12:16:22 +0000

    added support for uploading file

--- frontend/src/components/FileUpload.svelte
+++ frontend/src/components/FileUpload.svelte
@@ -1,12 +1,49 @@
-<div class="card compact bg-base-100">
-  <figure><img src="https://picsum.photos/id/1005/600/400" alt="img" /></figure>
-  <div class="flex-row items-center space-x-4 card-body">
+<script>
+  import Dropzone from 'svelte-file-dropzone'
+  import { createEventDispatcher } from 'svelte'
+
+  export let fileType = '*'
+  export let uploadText = 'Drop a file, or click to select a file'
+  export let title = 'Upload'
+
+  const dispatch = createEventDispatcher()
+  let progress = false
+
+  const handleFilesSelect = (e) => {
+    const { acceptedFiles } = e.detail
+    const reader = new FileReader()
+    if (acceptedFiles.length) {
+      reader.readAsArrayBuffer(acceptedFiles[0])
+      reader.onload = () => (progress = true)
+      reader.onloadend = (evt) => {
+        progress = false
+        if (evt.target.readyState === FileReader.DONE) {
+          dispatch('fileLoaded', evt.target.result)
+        }
+      }
+    }
+  }
+</script>
+
+<div class="card compact w-full h-full">
+  <div class="flex-col justify-start items-start space-x-4 card-body gap-2">
+    <Dropzone
+      on:drop={handleFilesSelect}
+      minSize={1}
+      accept={fileType}
+      multiple={false}
+      disableDefaultStyles={false}
+      containerClasses="w-full rounded-box h-full justify-center"
+    >
+      <p>{uploadText}</p>
+    </Dropzone>
     <div>
-      <h2 class="card-title">Upload Files</h2>
-      <p class="text-base-content text-opacity-40">
-        <button class="btn btn-ghost btn-sm btn-circle loading" />
-        loading
-      </p>
+      <h2 class="flex card-title">
+        {#if progress}
+          <p class="btn btn-ghost btn-sm btn-circle loading" />
+        {/if}
+        {title}
+      </h2>
     </div>
   </div>
 </div>
