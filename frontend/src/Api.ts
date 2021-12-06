10942805ab2899bce55461f1585c0ed8969c0e54
Ruthger Dijt <ruthgerdijt@gmail.com>
2021-12-06 14:06:44 +0100

    dont use env

--- frontend/src/Api.ts
+++ frontend/src/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-  baseURL: process.env.ENDPOINT || 'http://localhost:8000',
+  baseURL: 'http://localhost:8000',
 })
 
 export interface Classifier {
