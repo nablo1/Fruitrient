4d89862510d4af4fc1b901f451b219b9903379ed
bhavya <bhavya@student.chalmers.se>
2021-12-10 16:52:52 +0000

    Admin chart connected to backend
    Former-commit-id: 50a851711af6f2dda2494bc02b1f84df69a43042

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Fruitrient
 
 [![license](https://img.shields.io/badge/license-Apache--2.0-blue.svg)](./LICENSE)
+[![pipeline](https://git.chalmers.se/courses/dit825/2021/group07/fruitrient/badges/main/pipeline.svg)](https://git.chalmers.se/courses/dit825/2021/group07/fruitrient/-/commits/main)
 
 Fruitrient is an opensource project which helps users identify a fruit from a given image. Additionally, users can also view the nutritional information of the fruit.
 
--- frontend/src/pages/Admin.svelte
+++ frontend/src/pages/Admin.svelte
@@ -5,10 +5,17 @@
   import SelectCard from '../components/SelectCard.svelte'
   import SmallCard from '../components/SmallCard.svelte'
 
-  import { ActiveClassifier, active_classifier, active_classifier_history, Classifier, classifiers, set_active_classifier } from '../Api'
-  import { onMount } from 'svelte';
+  import {
+    ActiveClassifier,
+    active_classifier,
+    active_classifier_history,
+    Classifier,
+    classifiers,
+    set_active_classifier,
+  } from '../Api'
+  import { onMount } from 'svelte'
 
-  const titleOne = 'Latest Version'
+  const titleOne = 'Currently being used'
   const titleTwo = 'Accuracy'
   const textOne = 'V.5'
   const textTwo = '69,69%'
@@ -19,17 +26,21 @@
   let testing: any = undefined
 
   const update_stuff = async (testing: any) => {
-    console.log("update stuff")
-    if (testing && testing.id !== activeMl?.id ? await set_active_classifier(testing.id) : false) {
+    console.log('update stuff')
+    if (
+      testing && testing.id !== activeMl?.id
+        ? await set_active_classifier(testing.id)
+        : false
+    ) {
       activeMl = await active_classifier()
       activeMlHistory = await active_classifier_history()
     }
   }
-  
+
   $: update_stuff(testing)
-  $: console.log("activeMl: ", activeMl)
-  $: console.log("mlVersions: ", mlVersions)
-  $: console.log("activeMlHistory: ", activeMlHistory)
+  $: console.log('activeMl: ', activeMl)
+  $: console.log('mlVersions: ', mlVersions)
+  $: console.log('activeMlHistory: ', activeMlHistory)
 
   onMount(async () => {
     mlVersions = await classifiers()
@@ -53,10 +64,24 @@
     <NavBar />
   </div>
   <div class="shadow-lg rounded-box">
-    <SmallCard {titleOne} textOne={activeMl ? activeMl.classifier.name : "???"} textTwo={activeMl ? activeMl.classifier.performance + "%" : "???"} {titleTwo} />
+    <SmallCard
+      {titleOne}
+      textOne={activeMl ? activeMl.classifier.name : '???'}
+      textTwo={activeMl ? activeMl.classifier.performance + '%' : '???'}
+      {titleTwo}
+    />
   </div>
   <div class="shadow-lg rounded-box">
-    <SmallCard titleOne={'Previous Version'} textOne={activeMlHistory.length > 1 ? activeMlHistory[1]?.classifier.name : "???"} textTwo={ activeMlHistory.length > 1 ? activeMlHistory[1]?.classifier.performance + "%" : "???"} {titleTwo} />
+    <SmallCard
+      titleOne={'Previous Version'}
+      textOne={activeMlHistory.length > 1
+        ? activeMlHistory[1]?.classifier.name
+        : '???'}
+      textTwo={activeMlHistory.length > 1
+        ? activeMlHistory[1]?.classifier.performance + '%'
+        : '???'}
+      {titleTwo}
+    />
   </div>
   <div class="row-span-3 shadow-lg bg-base-100 rounded-box">
     <FileUpload
@@ -68,19 +93,26 @@
   </div>
   <div class="shadow-lg rounded-box">
     <SmallCard
-      titleOne={'Currently being used'}
-      {textOne}
-      {textTwo}
+      titleOne={'Latest Version'}
+      textOne={mlVersions.length > 0 ? mlVersions[0]?.name : '???'}
+      textTwo={mlVersions.length > 0 ? mlVersions[0]?.performance + '%' : '???'}
       {titleTwo}
     />
   </div>
   <div class="shadow-lg rounded-box">
-    <SelectCard bind:selected={testing} options={mlVersions} transform={el => el.id + " - " + el.name} />
+    <SelectCard
+      bind:selected={testing}
+      options={mlVersions}
+      transform={(el) => el.id + ' - ' + el.name}
+    />
   </div>
   <div class="card col-span-1 row-span-2 shadow-lg xl:col-span-2 bg-base-100">
     <div class="card-body">
       <h2 class="my-4 text-4xl font-bold card-title">Statistics</h2>
-      <Chart labels={mlVersions.map(el => el.name)} data={mlVersions.map(el => el.performance + 1)} />
+      <Chart
+        labels={mlVersions.map((el) => el.name)}
+        data={mlVersions.map((el) => el.performance)}
+      />
     </div>
   </div>
 </div>
